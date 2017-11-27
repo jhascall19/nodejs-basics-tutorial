b47408260ffee12b35e2c6ff315e5cbb24fb72bd
joshyhasc <jhascall19@Jhascall19.local>
2017-11-27 09:46:43 -0700

    Comments

--- 01-first-app/server.js
+++ 01-first-app/server.js
@@ -1,10 +1,10 @@
 //I WANNA 
 var http = require('http');
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+function onRequest(request, response) { //writing the function
+    response.writeHead(200, {'Content-Type': 'text/plain'}); // could set other header-keys as well
+    response.write('Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch'); //what will be printed
+    response.end(); //you are now done
 }
 
-http.createServer(onRequest).listen(8000);
+http.createServer(onRequest).listen(8000); //need to write the function on Request
