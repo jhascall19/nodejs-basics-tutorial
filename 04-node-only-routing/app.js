b9252fbd72e5c833ad1a6e9bb2f30fa2290409b7
joshyhasc <jhascall19@Jhascall19.local>
2017-11-29 09:14:03 -0700

    04

--- 04-node-only-routing/app.js
+++ 04-node-only-routing/app.js
@@ -1,4 +1,4 @@
-var url = require('url');
+var url = require('url'); //using the url to help find paths
 var fs = require('fs'); //exporting a function useful in node JS
 
 function renderHTML(path, response) {
@@ -14,21 +14,21 @@ function renderHTML(path, response) {
 }
 
 module.exports = {
-  handleRequest: function(request, response) {
+  handleRequest: function(request, response) { //same thing but in another file then index
       response.writeHead(200, {'Content-Type': 'text/html'});
 
-      var path = url.parse(request.url).pathname;
+      var path = url.parse(request.url).pathname; //this is finding the value for url whatever the user enters
       switch (path) {
           case '/':
-              renderHTML('./index.html', response);
+              renderHTML('./index.html', response); //two routes, each with the same response this time
               break;
           case '/login':
               renderHTML('./login.html', response);
               break;
           default:
               response.writeHead(404);
-              response.write('Route not defined');
-              response.end();
+              response.write('Route not defined'); //gives an error if neither
+              response.end(); //cuts out... it works!!!!
       }
 
   }
