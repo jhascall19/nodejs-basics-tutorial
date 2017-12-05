1820219362ede9b1704dc918cc4243682cf098b5
joshyhasc <jhascall19@Jhascall19.local>
2017-12-04 21:00:56 -0700

    07

--- 07-get-post/routes/index.js
+++ 07-get-post/routes/index.js
@@ -6,12 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
 });
 
-router.get('/test/:id', function(req, res, next) {
-  res.render('test', {output: req.params.id});
+router.get('/test/:id', function(req, res, next) { //this route expects this id it expects this parameter
+  res.render('test', {output: req.params.id}); //renders and passes the output as a variable tbat is the avaliable route of the .id.. you could aslo have others
 });
 
-router.post('/test/submit', function(req, res, next) {
+router.post('/test/submit', function(req, res, next) {  //this is a redirect. you are allowed to have a user input...
   res.redirect('/test/...');
 });
 
-module.exports = router;
+module.exports = router; //final export
